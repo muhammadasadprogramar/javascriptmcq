15cacb48fd17fef6aea9c8a4cc6f8085e4b1fd76
muhammadasadprogramar <muhammadasad852233@gmail.com>
2024-05-25 22:58:09 +0500

    javascript mcq

---
+++ app.js
@@ -0,0 +1,101 @@
+// Name
+
+let name = prompt("What is Your Name");
+if (name) {
+  alert("Your Name is " + name + " Submited");
+} else {
+  alert("Please enter your Name ⚠️");
+}
+
+//  Roll No
+
+let rollNo = prompt("What is Your Roll No");
+if (rollNo) {
+  alert("Your Roll no is " + rollNo + " Submited");
+} else {
+  alert("Please enter your Roll no ⚠️");
+}
+
+//  Queastion No 1
+
+let qNo1 = prompt(
+  "How many headings are there in HTMl?\n🟢 2\n🟢 4\n🟢 5\n🟢 6"
+);
+if (qNo1 == 6) {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ 6");
+}
+
+//  Queastion No 2
+
+let qNo2 = prompt(
+  "Which of these image does not have a background?\n🟢 jpg\n🟢 web\n🟢 png\n🟢 svg"
+);
+if (qNo2 == "png") {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ Png");
+}
+
+//  Queastion No 3
+
+let qNo3 = prompt(
+  "Which of these programing language is the most popular in the world?\n🟢 java\n🟢 C++\n🟢 javascript\n🟢 python"
+);
+if (qNo3 == "python") {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ Phthon");
+}
+
+//  Queastion No 4
+
+let qNo4 = prompt(
+  "Kiya aap ko JavaScript ab tak clear hy jo sir ne parhai hy?\n🟢 yes\n🟢 no"
+);
+if (qNo4 == "yes") {
+  alert("Good Hogaya Best of luck 👍");
+} else {
+  alert("To jao phir or dubra recorded classes dakho sir ki");
+}
+
+//  Queastion No 5
+
+let qNo5 = prompt(
+  "Which of these is the sign of modulus in JavaScript?\n🟢 =\n🟢 %\n🟢 $\n🟢 /"
+);
+if (qNo5 == "%") {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ %");
+}
+
+//  Queastion No 6
+
+let qNo6 = prompt(
+  "JavaScript was creared by?\n🟢 anders hejlsberg\n🟢 brendon mccullum\n🟢 bell labs\n🟢 brendan eich"
+);
+if (qNo6 == "brendan eich") {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ brendan eich");
+}
+
+//  Queastion No 7
+
+let qNo7 = prompt("(44 + 16) * 3\n🟢 230\n🟢 180\n🟢 166\n🟢 322");
+if (qNo7 == 180) {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ 180");
+}
+
+//  Queastion No 8
+
+let qNo8 = prompt("How mush is the reminder 28 % 3\n🟢 2\n🟢 8\n🟢 3\n🟢 5");
+if (qNo8 == 3) {
+  alert("Correct ✅");
+} else {
+  alert("Wrong ❌ 3");
+}
